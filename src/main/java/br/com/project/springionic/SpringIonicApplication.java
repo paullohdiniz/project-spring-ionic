ea2354ffcfe94f4bd8683f8ef67c14056b9833d6
paulodiniz <paulo.diniz@nativa.com.br>
2021-01-22 00:14:48 -0300

    Start do projeto

---
+++ src/main/java/br/com/project/springionic/SpringIonicApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.springionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIonicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/project/springionic/SpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.springionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIonicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
