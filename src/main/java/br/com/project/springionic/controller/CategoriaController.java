36e7a47d5f594ced7a2fdce5fac34f6770fabc4a
paullohdiniz <paullohdiniz@gmail.com.br>
2021-02-23 20:49:03 -0300

    Paginação de Categoria

--- src/main/java/br/com/project/springionic/controller/CategoriaController.java
+++ src/main/java/br/com/project/springionic/controller/CategoriaController.java
@@ -1,7 +1,9 @@
 package br.com.project.springionic.controller;
 
 import br.com.project.springionic.controller.domain.Categoria;
+import br.com.project.springionic.dto.CategoriaDTO;
 import br.com.project.springionic.services.CategoriaService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,17 +22,18 @@ public class CategoriaController {
     }
 
     @GetMapping
-    public List<Categoria> listar(){
-        return categoriaService.findAll();
+    public ResponseEntity<List<CategoriaDTO>> listar(){
+        List<CategoriaDTO> categorias = categoriaService.findAll();
+        return ResponseEntity.ok().body(categorias);
     }
 
-    @GetMapping(value = "{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Categoria> find(@PathVariable final Integer id){
         Categoria categoriaResponse = categoriaService.findById(id);
         return ResponseEntity.ok().body(categoriaResponse);
     }
 
-    @PostMapping(value = "{id}")
+    @PostMapping(value = "/{id}")
     public ResponseEntity<Void> salve(@RequestBody final Categoria categoria){
         Categoria categoriaResponse = categoriaService.insert(categoria);
 
@@ -43,15 +46,26 @@ public class CategoriaController {
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping(value = "{id}")
+    @PutMapping(value = "/{id}")
     public ResponseEntity<Void> update(@RequestBody final Categoria categoria){
         Categoria categoriaResponse = categoriaService.update(categoria);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping(value = "{id}")
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable final Integer id){
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Page<CategoriaDTO>> listarPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linePerPage", defaultValue = "10") Integer linePerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction
+    ){
+        Page<CategoriaDTO> categorias = categoriaService.findAllPerPage(page,linePerPage,orderBy,direction);
+        return ResponseEntity.ok().body(categorias);
     }
 }
--- src/main/java/br/com/project/springionic/dto/CategoriaDTO.java
+++ src/main/java/br/com/project/springionic/dto/CategoriaDTO.java
@@ -1,2 +1,18 @@
-package br.com.project.springionic.dto;public class CategoriaDTO {
+package br.com.project.springionic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
 }
