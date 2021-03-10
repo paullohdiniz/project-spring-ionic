8a5ccd41d290003e51ed367ba3c3ddc5f80f3b66
Paulo Diniz <paulo.diniz@kiman.com.br>
2021-03-09 22:09:51 -0300

    Add segurança

--- src/main/java/br/com/project/springionic/repository/ClienteRepository.java
+++ src/main/java/br/com/project/springionic/repository/ClienteRepository.java
@@ -4,6 +4,11 @@ import br.com.project.springionic.controller.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional
+    Cliente findByEmail(String email);
 }
--- src/main/java/br/com/project/springionic/services/ClienteService.java
+++ src/main/java/br/com/project/springionic/services/ClienteService.java
@@ -1,8 +1,10 @@
 package br.com.project.springionic.services;
 
-import br.com.project.springionic.controller.domain.Cliente;
+import br.com.project.springionic.controller.domain.*;
 import br.com.project.springionic.dto.ClienteDTO;
+import br.com.project.springionic.dto.ClienteNewDTO;
 import br.com.project.springionic.repository.ClienteRepository;
+import br.com.project.springionic.repository.EnderecoRepository;
 import br.com.project.springionic.services.exception.DataIntegrityException;
 import br.com.project.springionic.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +24,12 @@ public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder pe;
 
     public Cliente findById(Integer id){
         Optional<Cliente> obj = clienteRepository.findById(id);
@@ -35,15 +44,18 @@ public class ClienteService {
                 .map(cliente ->
                         new ClienteDTO(cliente.getId(),cliente.getNome(), cliente.getEmail())).collect(Collectors.toList());
     }
-    public Cliente insert(final Cliente cliente){
+
+    public Cliente insert(Cliente cliente){
         cliente.setId(null);
-        return clienteRepository.save(cliente);
+        cliente = clienteRepository.save(cliente);
+        enderecoRepository.saveAll(cliente.getEnderecos());
+        return cliente;
     }
 
-    public Cliente update(final Cliente cliente){
-        Cliente newCli = findById(cliente.getId());
-        udpateData(newCli, cliente);
-        return clienteRepository.save(newCli);
+    public Cliente update(Cliente cliente){
+        Cliente newObj = findById(cliente.getId());
+        udpateData(newObj, cliente);
+        return clienteRepository.save(newObj);
     }
 
     private void udpateData(Cliente newCli, Cliente cliente) {
@@ -66,6 +78,23 @@ public class ClienteService {
         Pageable pageRequest = PageRequest.of(page, linePerPage, Sort.Direction.valueOf(direction), orderBy);
         return clienteRepository.findAll(pageRequest)
                 .map(cliente -> new ClienteDTO(cliente.getId(), cliente.getNome(), cliente.getEmail()));
+    }
+
+    public Cliente fromDTONewClientes(ClienteNewDTO objDto) {
+        //TODO Ver se precisar instancia cidadeRepository
+        Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoClienteEnum.PESSOA_FISICA, pe.encode(objDto.getSenha()));
+        Estado estado = new Estado(null, "SP", null);
+        Cidade cid = new Cidade(objDto.getCidadeId(), "São Paulo", estado);
+        Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+        cli.getEnderecos().add(end);
+        cli.getTelefones().add(objDto.getTelefone1());
+        if (objDto.getTelefone2()!=null) {
+            cli.getTelefones().add(objDto.getTelefone2());
+        }
+        if (objDto.getTelefone3()!=null) {
+            cli.getTelefones().add(objDto.getTelefone3());
+        }
+        return cli;
     }
 
     public Cliente fromCliente(ClienteDTO clienteDTO){
